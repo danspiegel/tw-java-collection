8c97f90362548e44acebc7058c6b243d38ef9cf6
Daniel Spiegel <danispiegel@gmail.com>
2019-11-12 15:26:37 -0300

    Organizando os repositórios

---
+++ src/br/com/treinaweb/java/collections/MainSet.java
@@ -0,0 +1,31 @@
+package br.com.treinaweb.java.collections;
+
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+import br.com.treinaweb.java.collectioms.models.Pessoa;
+
+public class MainSet {
+
+	public static void main(String[] args) {
+		NavigableSet<Pessoa> set = new TreeSet<Pessoa>();
+		set.add(new Pessoa(2, "TreinaWeb 2"));
+		set.add(new Pessoa(3, "TreinaWeb 3"));
+		set.add(new Pessoa(4, "TreinaWeb 4"));
+		set.add(new Pessoa(1, "Abel"));
+		Pessoa teste = set.lower(new Pessoa(2, "TreinaWeb 2"));
+		System.out.println(teste);
+		set.add(null);
+//		System.out.println(set);		
+//		set.add(new Pessoa(2, "TreinaWeb 2"));
+//		System.out.println(set);
+//		for (Pessoa p : set) {
+//			System.out.println(p);
+//		}
+		// ********
+		//| Chave | Valor                       |
+		//---------------------------------------
+		//| 123   | new Pessoa(1, "TreinaWeb 1")|
+	}
+
+}
